cc731b165fb5f6b9a6f1bf2d91baf1d0fe869701
Eric Yang <4650519+domorethings@users.noreply.github.com>
2018-05-22 18:31:47 -0700

    Added more note functionality
    Added a random note and the ability to add more notes to the board

--- src/Board.js
+++ src/Board.js
@@ -1,43 +1,71 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import FaPlus from 'react-icons/lib/fa/plus'
 
 class Board extends Component {
   constructor(props){
     super(props)
     this.state = {
-      notes: [
-        {
-          id: 0,
-          note: 'Call Mom'
-        },
-        {
-          id: 1,
-          note: "Get a car wash"
-        },
-        {
-          id: 2,
-          note: "Love Pookie"
-        }
-      ]
+      notes: []
     }
+    this.add = this.add.bind(this)
     this.eachNote = this.eachNote.bind(this)
     this.update = this.update.bind(this)
+    this.remove = this.remove.bind(this)
+    this.nextId = this.nextId.bind(this)
   }
 
-  update(newText, i) {
-    console.log('updating item at index ', i, newText)
-    this.setState(prevState => ({
-      notes: prevState.notes.map(
-        note => ( note.id !== i ) ? note : {...note, note: newText}
-      )
+  componentWillMount(){
+    var self = this
+    if (this.props.count) {
+      fetch('https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}')
+        .then(response => response.json())
+        .then(json => json[0] 
+            .split('. ')
+            .forEach(sentence => self.add(sentence.substring(0, 25))))
+    }
+  }
+
+  nextId() {
+    this.uniqueId = this.uniqueId || 0
+    return this.uniqueId++
+  }
+
+  add(text) {
+    this.setState(prevState =>({
+      notes: [
+        ...prevState.notes, {
+          id: this.nextId(),
+          note: text
+        }
+      ]
     }))
   }
 
+	update(newText, i) {
+	  console.log('updating item at index', i, newText)
+	  this.setState(prevState => ( {
+	    notes: prevState.notes.map(
+	      note => (note.id !== i) ? note : { ...note,
+	        note: newText
+	      }
+	    )
+	  }))
+	}
+
+	remove(id) {
+	  console.log('removing item at', id)
+	  this.setState(prevState => ({
+	    notes: prevState.notes.filter(note => note.id !== id)
+	  }))
+	}
+
   eachNote(note, i){
     return (
-      <Note key={i}
-            index={i}
-            onChange={this.update}>
+      <Note key={note.id}
+            index={note.id}
+            onChange={this.update}
+            onRemove={this.remove}>
             {note.note}
           </Note>
     )
@@ -47,7 +75,11 @@ class Board extends Component {
     return(
       <div className='board'>
         {this.state.notes.map(this.eachNote)}
-      </div> 
+        <button onClick={this.add.bind(null, "New Sticky")}
+              id="add">
+            <FaPlus />
+          </button>
+      </div>
     )
   }
 }
